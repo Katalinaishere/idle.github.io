1afb524833101711aea0255eb6906c1b437e388b
Katalina <47336744+Katalinaishere@users.noreply.github.com>
2023-09-02 13:16:17 +0200

    Update styles.cs

--- styles.cs
+++ styles.cs
@@ -6,3 +6,12 @@ body{
     color: #e6e6e6;
  }
 }
+html, body {
+    margin: 0;
+    background-color: #ffe8d6;
+    height: 100%;
+    font-family: new-spirit,serif;
+    font-weight: 400;
+    font-style: normal;
+    transition: background-color .5s ease-in-out;
+}
