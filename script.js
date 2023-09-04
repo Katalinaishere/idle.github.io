ed707d8aa68a44daa84176522057cc478dd084f9
Katalina <47336744+Katalinaishere@users.noreply.github.com>
2023-09-04 22:11:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -234,7 +234,7 @@ function updateClock (){
     const now = new Date(); 
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSecond().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
     const timeString = '${hours}:${minutes}:${seconds}';
     clock.textContent = timeString;   
 }
